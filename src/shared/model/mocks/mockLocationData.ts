dd38f2bd3a0dbdc1395941d848d6b5ac1f04cd95
RohSungKyun <songkyun1108@naver.com>
2025-04-09 17:18:13 +0900

    [chore] #1 : strictmode 제거

---
+++ src/shared/model/mocks/mockLocationData.ts
@@ -0,0 +1,29 @@
+export const mockLocationData = {
+    locations: [
+      {
+        id: "A",
+        name: "서울시청",
+        lat: 37.5665,
+        lng: 126.9780,
+      },
+      {
+        id: "B",
+        name: "마포구청",
+        lat: 37.5796,
+        lng: 126.8904,
+      },
+      {
+        id: "C",
+        name: "건대입구역",
+        lat: 37.5512,
+        lng: 127.0732,
+      },
+    ],
+    midpoint: {
+      id: "MID",
+      name: "중간 지점",
+      lat: 37.5658,
+      lng: 126.9805,
+    },
+  };
+  
