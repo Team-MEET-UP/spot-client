c124128c7d4a60e5714325af9ec9442a8016bce6
yeona813 <aya813@naver.com>
2025-04-26 00:07:17 +0900

    [feat] #8 : chip UI 구현

---
+++ src/features/my/ui/Chip.tsx
@@ -0,0 +1,15 @@
+interface ChipProps {
+  isComplete: boolean;
+}
+
+const Chip = ({ isComplete }: ChipProps) => {
+  return (
+    <div
+      className={`flex gap-[2px] px-3 py-1 w-fit rounded-[40px] ${isComplete ? "bg-gray-5 text-gray-30" : "bg-sub-10 text-sub-sub"}`}>
+      <img src={isComplete ? "/icon/check.svg" : "/icon/review.svg"} alt="icon" />
+      {isComplete ? "작성완료" : "리뷰작성"}
+    </div>
+  );
+};
+
+export default Chip;
