bec338f7fac7fe19e126ffb3292d7838030b12e5
yeona813 <aya813@naver.com>
2025-04-26 00:08:17 +0900

    [feat] #8 : list UI 구현

---
+++ src/features/my/ui/List.tsx
@@ -0,0 +1,57 @@
+import Chip from "./Chip";
+
+interface ListProps {
+  midPoint: string;
+  isSelect?: boolean;
+  place?: string;
+  imgUrl: string[];
+  people: number;
+  day: number;
+  isComplete: boolean;
+}
+
+const List = ({ midPoint, isSelect = true, place, imgUrl, people, day, isComplete }: ListProps) => {
+  const displayImages = imgUrl.slice(0, 3);
+
+  return (
+    <section className="flex flex-col pb-5 pt-4 gap-1">
+      <span className="text-md font-semibold text-gray-90">{midPoint}</span>
+      <div className="flex gap-1 items-center text-sm font-medium text-gray-40 overflow-hidden text-ellipsis whitespace-nowrap">
+        {isSelect ? (
+          <>
+            <img src="/icon/pin.svg" alt="pin" />
+            <p>{place}</p>
+          </>
+        ) : (
+          <p>아직 장소가 정해지지 않았어요</p>
+        )}
+      </div>
+      <div className="flex mt-1 items-center justify-between">
+        <div className="flex gap-1 text-xs font-medium">
+          {/* 이미지 영역 */}
+          <div className="flex items-center">
+            {displayImages.map((src, index) => {
+              const zIndex = 30 - index * 10;
+              const marginClass = index === 0 ? "" : "-ml-[7px]";
+              return (
+                <img
+                  key={index}
+                  src={src}
+                  className={`w-[21px] h-[21px] rounded-full border border-white ${marginClass}`}
+                  style={{ zIndex }}
+                />
+              );
+            })}
+          </div>
+
+          <p className="text-gray-90">{people}명</p>
+          <p className="font-semibold text-gray-40">·</p>
+          <p className="text-gray-40">{day}일 전</p>
+        </div>
+        <Chip isComplete={isComplete} />
+      </div>
+    </section>
+  );
+};
+
+export default List;
---
+++ src/features/my/ui/index.ts
@@ -0,0 +1,5 @@
+export * from "./Header";
+export * from "./Banner";
+export * from "./Empty";
+export * from "./Chip";
+export * from "./List";
