687b6c825a6126e89e4b31976aa1488c6d12abba
yeona813 <aya813@naver.com>
2025-04-25 22:00:04 +0900

    [feat] #5 : 공통 버튼 구현

--- src/features/main/ui/KakaoLogin.tsx
+++ src/features/main/ui/KakaoLogin.tsx
@@ -1,3 +1,4 @@
+//@TODO 실제 카카오톡 로그인 기능 구현!
 const KakaoLogin = () => {
   return (
     <button className="flex gap-2 w-[320px] h-14 justify-center items-center bg-kakao-yellow text-kakao-black text-md font-semibold rounded-2xl">
