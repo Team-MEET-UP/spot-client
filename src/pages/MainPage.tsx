6c6aa093f6606e859d426e19812abad80b2b8cb4
yeona813 <aya813@naver.com>
2025-04-25 22:00:23 +0900

    [feat] #5 : main 페이지 ui 구현

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,23 @@
-const MainPage = () => {
-  return <div className="bg-gray-50">메인ㅎㅎ</div>;
+import KakaoLogin from "@/features/main/ui/KakaoLogin";
+import Button from "@/shared/ui/Button";
+import { useNavigate } from "react-router-dom";
 
+const MainPage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col gap-3 items-center pb-6 justify-end h-screen">
+      <span className="text-md font-medium text-gray-60">모임의 기준을 만들다</span>
+      <img src="/icon/logo.svg" alt="logo" className="mb-[38px]" />
+      <Button
+        onClick={() => {
+          navigate("/find");
+        }}>
+        바로 모임 만들기
+      </Button>
+      <KakaoLogin />
+    </div>
+  );
 };
 
 export default MainPage;
