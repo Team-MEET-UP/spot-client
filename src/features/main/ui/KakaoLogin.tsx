b5130de3dd4629bd26a0f8e2d61c18582cf6446a
yeona813 <aya813@naver.com>
2025-04-25 21:57:50 +0900

    [feat] #5 : 카카오 로그인 버튼 구현

---
+++ src/features/main/ui/KakaoLogin.tsx
@@ -0,0 +1,10 @@
+const KakaoLogin = () => {
+  return (
+    <button className="flex gap-2 w-[320px] h-14 justify-center items-center bg-kakao-yellow text-kakao-black text-md font-semibold rounded-2xl">
+      <img src="/icon/kakao.svg" alt="카카오 로그인" className="w-5 h-5" />
+      카카오 로그인
+    </button>
+  );
+};
+
+export default KakaoLogin;
