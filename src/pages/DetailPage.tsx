23b97134fd4aee110d00d7d6f223afb9df10a094
yeona813 <aya813@naver.com>
2025-04-26 17:06:04 +0900

    [refactor] #10 : 잘못 올라간 페이지 수정 삭제

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,16 +1,5 @@
-import BackHeader from "@/shared/ui/BackHeader";
-import CloseHeader from "@/shared/ui/CloseHeader";
-import PlainHeader from "@/shared/ui/PlainHeader";
-
 const DetailPage = () => {
-  return (
-    <div>
-      디테일
-      <PlainHeader title="멤버 추가" url="/" />
-      <BackHeader />
-      <CloseHeader />
-    </div>
-  );
+  return <div>디테일</div>;
 };
 
 export default DetailPage;
