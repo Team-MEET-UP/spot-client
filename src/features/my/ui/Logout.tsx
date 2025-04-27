86dd19658478d43d03104a604944dcc197b2511e
yeona813 <aya813@naver.com>
2025-04-27 11:51:29 +0900

    [feat] #13 : 로그아웃 ui 구현

---
+++ src/features/my/ui/Logout.tsx
@@ -0,0 +1,10 @@
+const Logout = () => {
+  return (
+    <button className="w-full h-[44px] flex gap-2 items-center justify-center bg-gray-5 rounded-xl">
+      <img src="/icon/logout.svg" alt="logout" />
+      <p className="text-md font-medium text-gray-60">로그아웃</p>
+    </button>
+  );
+};
+
+export default Logout;
