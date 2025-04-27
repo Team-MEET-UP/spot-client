75a6f0ac92d8bd9a88127b203413e0adc6d3fb2d
yeona813 <aya813@naver.com>
2025-04-27 21:24:31 +0900

    [feat] #15 : 리뷰 작성 페이지 이동 추가

--- src/features/history/ui/Chip.tsx
+++ src/features/history/ui/Chip.tsx
@@ -11,7 +11,11 @@ const Chip = ({ id, isComplete }: ChipProps) => {
   return (
     <button
       className={`flex gap-[2px] px-3 py-1 w-fit rounded-[40px] ${isComplete ? "bg-gray-5 text-gray-30" : "bg-sub-10 text-sub-sub"}`}
-      onClick={() => navigate(`/review/${id}`)}>
+      onClick={() => {
+        if (isComplete) return;
+
+        navigate(`/review/${id}`);
+      }}>
       <img src={isComplete ? "/icon/check.svg" : "/icon/review.svg"} alt="icon" />
       {isComplete ? "작성완료" : "리뷰작성"}
     </button>
