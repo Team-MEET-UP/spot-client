c16a3c71c12999fa2535254c77387988e04fe10f
yeona813 <aya813@naver.com>
2025-04-27 11:51:45 +0900

    [feat] #13 : 마이페이지 ui 구현

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,5 +1,20 @@
+import Logout from "@/features/my/ui/Logout";
+import Menu from "@/features/my/ui/Menu";
+import Profile from "@/features/my/ui/Profile";
+import PlainHeader from "@/shared/ui/PlainHeader";
+
 const MyPage = () => {
-  return <div>마이페이지</div>;
+  return (
+    <div className="flex flex-col">
+      <PlainHeader url="/history" title="내 정보" />
+      <div className="flex flex-col px-5 gap-2 mb-8">
+        <Profile />
+        <Logout />
+      </div>
+      <div className="w-full h-2 bg-gray-5" />
+      <Menu />
+    </div>
+  );
 };
 
 export default MyPage;
