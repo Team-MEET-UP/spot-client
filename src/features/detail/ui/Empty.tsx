39cee6371c07501c3c9b362a40e1c1d48d059402
yeona813 <aya813@naver.com>
2025-04-28 16:34:05 +0900

    [feat] #17 : 리뷰를 작성하지 않은 경우 ui

---
+++ src/features/detail/ui/Empty.tsx
@@ -0,0 +1,14 @@
+const Empty = () => {
+  return (
+    <div className="p-5 pt-[26px] flex flex-col items-center">
+      <div className="w-[160px] h-[160px] bg-gray-30" /> {/* 임시 이미지 영역*/}
+      <span className="mt-4 text-md font-semibold text-gray-40">아직 후기가 없어요</span>
+      <p className="text-sm font-medium text-gray-30">
+        해당 가게를 방문 후 후기를 남겨주시면
+        <br /> 다른 방문자들에게 큰 도움이 될거에요
+      </p>
+    </div>
+  );
+};
+
+export default Empty;
