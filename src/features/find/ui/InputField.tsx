742ce8fae2bd7366ae98621608445ee76aa61983
RohSungKyun <songkyun1108@naver.com>
2025-04-28 23:14:23 +0900

    [chore] #6 : 너비 조정

--- src/features/find/ui/InputField.tsx
+++ src/features/find/ui/InputField.tsx
@@ -7,13 +7,13 @@ interface InputFieldProps {
 
 const InputField = ({ value, onChange, placeholder, error }: InputFieldProps) => {
   return (
-    <div className="relative">
+    <div className="relative w-full">
       <input
         type="text"
         value={value}
         onChange={onChange}
         placeholder={placeholder}
-        className="w-[320px] py-[8px] my-[16px] border-b border-gray-20 placeholder:text-gray-20 focus:border-gray-80 focus:placeholder:text-gray-80 placeholder:text-lg focus:outline-none focus:text-gray-90 pr-8"
+        className="w-full py-[8px] my-[16px] border-b border-gray-20 placeholder:text-gray-20 focus:border-gray-80 focus:placeholder:text-gray-80 placeholder:text-lg focus:outline-none focus:text-gray-90 pr-8"
       />
       <img src="./icon/search.svg" alt="search" className="absolute right-0 top-1/2 -translate-y-1/2" />
       {error && <p className="text-error text-xs">{error}</p>}
--- src/pages/FindPage.tsx
+++ src/pages/FindPage.tsx
@@ -2,7 +2,7 @@ import { FindContainer } from "@/features/find/ui";
 
 const FindPage = () => {
   return (
-    <div className="flex flex-col items-center h-screen">
+    <div className="flex flex-col h-screen">
       <FindContainer />
     </div>
   );
