1bae63201b8c117beea5d56174ebb56bee877ee0
RohSungKyun <songkyun1108@naver.com>
2025-04-30 20:49:17 +0900

    [feat] #19 : mp페이지 데이터 store 구현(임시)

---
+++ src/features/mapView/ui/BottomSheetContent.tsx
@@ -0,0 +1,8 @@
+
+export const BottomSheetContent = () => {
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+};
--- src/shared/stores/index.ts
+++ src/shared/stores/index.ts
@@ -1,1 +1,2 @@
 export * from "./useFindStore";
+export * from "./useMapStore";
