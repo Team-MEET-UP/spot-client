4c8f56c25b131cd0c849c06efbb59605d961f9f0
RohSungKyun <songkyun1108@naver.com>
2025-04-30 23:40:24 +0900

    [feat] #19 : 모임 장소 정하기 버튼 구현

--- src/features/mapView/ui/MapBottomSheet.tsx
+++ src/features/mapView/ui/MapBottomSheet.tsx
@@ -5,7 +5,12 @@ export const MapBottomSheet = () => {
   return (
     <>
       <BottomSheet minHeightVh={35} maxHeightVh={80}>
-        <BottomSheet.Header />
+        <div className="relative">
+          <BottomSheet.Header />
+          <button className="absolute -top-14 left-1/2 -translate-x-1/2 bg-gray-80 text-white font-semibold text-md px-[24px] py-[12px] rounded-3xl">
+            모임 장소 정하기
+          </button>
+        </div>
         <BottomSheet.Content>
           <BottomSheetContent />
           <FixedButtons />
--- src/shared/ui/BottomSheet/Wrapper.tsx
+++ src/shared/ui/BottomSheet/Wrapper.tsx
@@ -6,12 +6,6 @@ export const Wrapper = forwardRef<HTMLDivElement, WrapperProps>(({ children, ...
   <div
     ref={ref}
     className="fixed left-0 right-0 bottom-0 z-[1000] bg-white rounded-t-2xl shadow-lg transition-transform duration-200 flex flex-col"
-    style={{
-      touchAction: 'pan-y',
-      WebkitTouchCallout: 'none',
-      WebkitUserSelect: 'none',
-      userSelect: 'none'
-    }}
     {...props}>
     {children}
   </div>
