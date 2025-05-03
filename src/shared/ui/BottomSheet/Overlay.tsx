efd28ffa50f95ceea689ecdcda602ba5988a2706
yeona813 <aya813@naver.com>
2025-05-03 13:21:07 +0900

    [feat] #31 : PolicyBottomSheet UI 구현

--- src/shared/ui/BottomSheet/Overlay.tsx
+++ src/shared/ui/BottomSheet/Overlay.tsx
@@ -1,9 +1,13 @@
 interface OverlayProps {
   onClick?: () => void;
+  isBlur?: boolean;
 }
 
-export const Overlay = ({ onClick }: OverlayProps) => {
+export const Overlay = ({ onClick, isBlur = true }: OverlayProps) => {
   return (
-    <div className="fixed inset-0 bg-black/50 transition-opacity duration-200 backdrop-blur-sm" onClick={onClick} />
+    <div
+      className={`fixed inset-0 bg-black/50 transition-opacity duration-200 ${isBlur && "backdrop-blur-sm"}`}
+      onClick={onClick}
+    />
   );
 };
