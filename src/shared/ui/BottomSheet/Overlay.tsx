452b01664ea7a4486b98f3743c582f1eab6b88c6
yeona813 <aya813@naver.com>
2025-05-03 13:39:31 +0900

    [refactor] #31 : 이미지 z-index 수정

--- src/shared/ui/BottomSheet/Overlay.tsx
+++ src/shared/ui/BottomSheet/Overlay.tsx
@@ -6,7 +6,7 @@ interface OverlayProps {
 export const Overlay = ({ onClick, isBlur = true }: OverlayProps) => {
   return (
     <div
-      className={`fixed inset-0 bg-black/50 transition-opacity duration-200 ${isBlur && "backdrop-blur-sm"}`}
+      className={`fixed inset-0 bg-black/50 duration-200 transition-opacity z-[100] ${isBlur ? "backdrop-blur-sm" : ""}`}
       onClick={onClick}
     />
   );
