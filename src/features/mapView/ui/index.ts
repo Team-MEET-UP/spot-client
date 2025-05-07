56746500ae9cde9bd36d824d0947e2e2eeceed86
yeona813 <aya813@naver.com>
2025-05-07 09:46:58 +0900

    [feat] #35 : detailKakaoMap 구현

--- src/features/mapView/ui/index.ts
+++ src/features/mapView/ui/index.ts
@@ -1,7 +1,6 @@
 export * from "./KakaoMapView";
 export * from "./MapHeader";
-export * from "./MapBottomSheet";
 export * from "./AddMemberBottomSheet";
 export * from "./MapDetailBottomSheet";
-
 export * from "./SnapBottomSheet";
+export * from "./DetailKakaoMapView";
