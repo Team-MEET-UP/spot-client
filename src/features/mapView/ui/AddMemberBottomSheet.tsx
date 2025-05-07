093fba85c9bdf4d7af252b1458dd954ee9100920
RohSungKyun <songkyun1108@naver.com>
2025-05-08 00:54:33 +0900

    [style] #39 : mp 그래픽 적용

--- src/features/mapView/ui/AddMemberBottomSheet.tsx
+++ src/features/mapView/ui/AddMemberBottomSheet.tsx
@@ -6,12 +6,13 @@ export const AddMemberBottomSheet = () => {
   return (
     <>
       <Overlay />
-      <BottomSheet minHeightVh={40} maxHeightVh={40}>
+      <BottomSheet minHeightVh={60} maxHeightVh={60}>
         <BottomSheet.Header />
         <BottomSheet.Content>
           <div className="flex flex-col justify-center items-center h-full">
-            <h1 className="text-gray-80 font-bold text-lg">멤버를 추가해보세요</h1>
-            <p className="text-gray-40 text-sm">중간 지점을 찾으려면 멤버가 더 필요해요</p>
+            <img src="/icon/addMemberRequired.svg" alt="멤버를 추가 필요" />
+            <h1 className="text-gray-80 font-bold text-lg">멤버를 추가해주세요</h1>
+            <p className="text-gray-40 text-sm">직접 추가하거나, 링크를 공유할 수 있어요</p>
           </div>
           <FixedButtons />
         </BottomSheet.Content>
--- src/pages/MapViewPage.tsx
+++ src/pages/MapViewPage.tsx
@@ -8,15 +8,13 @@ import {
 } from "@/features/mapView/ui";
 
 const MapViewPage = () => {
-  const isDetail = true; // 임시 작업
+  const isDetail = false; // 임시 작업
   const openDetailBottomSheet = true; // 임시 작업
-  const memberCount = 2; // 임시 인원 설정
+  const memberCount = 1; // 임시 인원 설정
 
   return (
     <div>
-      <div className="px-5">
-        <MapHeader />
-      </div>
+      <MapHeader />
       {isDetail ? (
         <>
           <DetailKakaoMapView />
