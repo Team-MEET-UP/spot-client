448088689655a8986cdc33372135866fd9c92d69
yeona813 <aya813@naver.com>
2025-05-08 22:45:52 +0900

    [fix] #47 : qa cta 버튼 하단 고정

--- src/pages/VisitedPage.tsx
+++ src/pages/VisitedPage.tsx
@@ -35,16 +35,20 @@ const VisitedPage = () => {
   };
 
   return (
-    <div className="flex flex-col h-screen-dvh">
+    <div className="relative flex flex-col h-screen-dvh">
       <BackHeader onClick={handleBack} />
       <div className="flex flex-col justify-between h-full p-5 pt-2">
         <div>
           <StepIndicator step={currentStep} />
           <span className="mt-2 text-lg font-bold text-gray-70">이 장소는 어떠셨나요?</span>
           <p className="text-md font-medium text-gray-40">이용 경험에 대해 알려주세요</p>
-          {currentStep === 1 && <FirstStep selectedTime={selectedTime} setSelectedTime={setSelectedTime} />}
-          {currentStep === 2 && <SecondStep secondData={secondData} setSecondData={setSecondData} />}
+          <div className="flex-1 overflow-y-auto max-h-[calc(100vh-220px)] scrollbar-hidden">
+            {currentStep === 1 && <FirstStep selectedTime={selectedTime} setSelectedTime={setSelectedTime} />}
+            {currentStep === 2 && <SecondStep secondData={secondData} setSecondData={setSecondData} />}
+          </div>
         </div>
+      </div>
+      <div className="fixed bottom-5 left-5 w-[calc(100%-40px)]">
         <Button
           onClick={currentStep === 1 ? handleFirstStep : handleSecondStep}
           disabled={
