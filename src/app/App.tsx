b992fa76fe6982391d228641754b9df5b6d8b35c
RohSungKyun <songkyun1108@naver.com>
2025-05-09 03:32:12 +0900

    [chore] #45 : react cookie 설치

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,17 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { CookiesProvider } from "react-cookie";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <CookiesProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </CookiesProvider>
   );
 }
 
