d0d5e65b519481f5aa9f15cfb1febdd0fef27f43
yeona813 <aya813@naver.com>
2025-05-08 21:56:30 +0900

    [feat] #18 : 카카오톡 소셜 로그인 구현 및 그래픽 추

--- src/features/main/ui/KakaoLogin.tsx
+++ src/features/main/ui/KakaoLogin.tsx
@@ -1,7 +1,13 @@
-//@TODO 실제 카카오톡 로그인 기능 구현!
 export const KakaoLogin = () => {
+  const handleKakaoLogin = () => {
+    const kakaoAuthUrl = "http://211.188.57.205:8080/oauth2/authorization/kakao";
+    window.location.href = kakaoAuthUrl;
+  };
+
   return (
-    <button className="flex gap-2 w-full h-14 justify-center items-center bg-kakao-yellow text-kakao-black text-md font-semibold rounded-2xl">
+    <button
+      className="flex gap-2 w-full h-14 justify-center items-center bg-kakao-yellow text-kakao-black text-md font-semibold rounded-2xl"
+      onClick={handleKakaoLogin}>
       <img src="/icon/kakao.svg" alt="카카오 로그인" className="w-5 h-5" />
       카카오 로그인
     </button>
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -8,7 +8,7 @@ const MainPage = () => {
   return (
     <div className="relative bg-[#E5EFF7] h-screen-dvh flex flex-col justify-end">
       <div
-        className="flex flex-col gap-3 items-center pb-6 h-fit px-5 pt-32"
+        className="flex flex-col gap-3 items-center pb-6 h-fit px-5 pt-32 z-10"
         style={{ background: "linear-gradient(180deg, rgba(255, 255, 255, 0.00) 0%, #FFF 30%)" }}>
         <span className="text-md font-medium text-gray-60">모임의 기준을 만들다</span>
         <img src="/icon/logo.svg" alt="logo" className="mb-[38px]" />
@@ -20,7 +20,7 @@ const MainPage = () => {
         </Button>
         <KakaoLogin />
       </div>
-      <img src="/icon/main.svg" alt="main" className="absolute top-16 right-0 " />
+      <img src="/image/main.png" alt="main" className="absolute top-16 right-0" />
     </div>
   );
 };
