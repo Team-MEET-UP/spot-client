f63a39660689c26eed85698daec58d82dda3d132
RohSungKyun <songkyun1108@naver.com>
2025-05-08 13:00:17 +0900

    [chore] #39 : 코드 패턴 수정

--- src/shared/ui/InputField.tsx
+++ src/shared/ui/InputField.tsx
@@ -16,10 +16,8 @@ export const InputField = ({ value, onChange, placeholder, error, type }: InputF
         placeholder={placeholder}
         className="w-full py-[8px] my-[16px] border-b border-gray-20 placeholder:text-gray-20 focus:border-gray-80 focus:placeholder-transparent placeholder:text-lg focus:outline-none focus:text-gray-90 pr-8 rounded-none"
       />
-      {type === "startPoint" ? (
+      {type === "startPoint" && (
         <img src="./icon/search.svg" alt="search" className="absolute right-0 top-1/2 -translate-y-1/2" />
-      ) : (
-        <></>
       )}
       {error && <p className="text-error text-xs">{error}</p>}
     </div>
