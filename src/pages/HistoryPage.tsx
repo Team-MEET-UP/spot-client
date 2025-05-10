5f19b1165bbc7a5464834032cf43d8017f811c45
yeona813 <aya813@naver.com>
2025-05-11 01:24:15 +0900

    [feat] #52 : 유저 정보 조회에 따른 약관 정보 바텀 시트 관리

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -1,19 +1,37 @@
+import { getUserInfo } from "@/features/history/service";
 import { Banner, Empty, Header, GroupCard, PolicyBottomSheet } from "@/features/history/ui";
 import { mockListData } from "@/shared/model";
-import { useState } from "react";
+import { useUserStore } from "@/shared/stores";
+import { useEffect, useState } from "react";
 
 const HistoryPage = () => {
+  const [isPolicy, setIsPolicy] = useState(false);
+  const { profileImageUrl } = useUserStore();
   const length = 1; // 임시 ui 구현을 위한 작업!
-  const [isPolicy, setIsPolicy] = useState(true);
 
-  const handleClick = () => {
+  const onClose = () => {
     setIsPolicy(false);
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getUserInfo();
+      if (response.data) {
+        useUserStore.setState({
+          nickname: response.data.nickname,
+          profileImageUrl: response.data.profileImageUrl,
+        });
+        setIsPolicy(!response.data.personalInfoAgreement);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="flex flex-col h-screen-dvh">
       <div className="flex flex-col px-5 gap-2">
-        <Header profileImg="https://github.com/shadcn.png" />
+        <Header profileImg={profileImageUrl} />
         <Banner />
         <span className="mt-4 py-3 text-lg font-bold">나의 모임</span>
       </div>
@@ -26,7 +44,7 @@ const HistoryPage = () => {
       ) : (
         <Empty />
       )}
-      {isPolicy && <PolicyBottomSheet onClose={handleClick} />}
+      {isPolicy && <PolicyBottomSheet onClose={onClose} />}
     </div>
   );
 };
