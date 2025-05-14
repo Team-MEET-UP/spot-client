69cacdde799cd54ac11f40b085383640a5ad0a11
RohSungKyun <songkyun1108@naver.com>
2025-05-15 02:50:58 +0900

    [hotfix] : 유저 정보 저장 후 리디렉션 처리

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -17,21 +17,22 @@ const HistoryPage = () => {
   };
 
   useEffect(() => {
-    const eventIdFromStorage = localStorage.getItem("shared_link_access");
-    if (eventIdFromStorage) {
-      navigate(`/find?eventId=${eventIdFromStorage}&startStep=1`);
-    }
-  }, [navigate]);
-
-  useEffect(() => {
     if (data) {
+      // 데이터가 로드된 후 사용자 정보 저장
       useUserStore.setState({
         nickname: data.nickname,
         profileImageUrl: data.profileImageUrl,
       });
+
       setIsPolicy(!data.personalInfoAgreement);
+
+      // localStorage에서 이벤트 아이디를 가져와 리디렉션 처리
+      const eventIdFromStorage = localStorage.getItem("shared_link_access");
+      if (eventIdFromStorage) {
+        navigate(`/find?eventId=${eventIdFromStorage}&startStep=1`);
+      }
     }
-  }, [data]);
+  }, [data, navigate]); // 데이터가 변경되면 실행
 
   if (isLoading) return <p>로딩 중...</p>;
   if (isError) return <p>유저 정보를 가져오는 데 실패했습니다.</p>;
