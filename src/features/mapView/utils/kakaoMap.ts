dab69c49b8173ffca062abb351ebdb67bbe8e4ad
yeona813 <aya813@naver.com>
2025-05-14 21:56:03 +0900

    [refactor] : 주석 수정

--- src/features/mapView/utils/kakaoMap.ts
+++ src/features/mapView/utils/kakaoMap.ts
@@ -10,7 +10,7 @@ export const openKakaoMap = ({ startLat, startLng, endPoint, endLat, endLng }: O
   const kakaoMapUrl = `https://map.kakao.com/link/to/${endPoint},${endLat},${endLng}`;
   window.open(kakaoAppUrl, "_blank");
 
-  // APP이 설치되어 있지 않으면 웹으로 리다이렉트트
+  // APP이 설치되어 있지 않으면 웹으로 리다이렉트
   setTimeout(() => {
     if (!document.hidden) {
       window.open(kakaoMapUrl, "_blank");
