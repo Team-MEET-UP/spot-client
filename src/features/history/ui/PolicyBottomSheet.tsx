ebc846773131634bf0ad0d3e19011872abf2883c
RohSungKyun <songkyun1108@naver.com>
2025-05-14 22:26:48 +0900

    [fix] #62 : 약관 시트 수정

--- src/features/history/ui/PolicyBottomSheet.tsx
+++ src/features/history/ui/PolicyBottomSheet.tsx
@@ -39,7 +39,7 @@ export const PolicyBottomSheet = ({ onClose }: PolicyBottomSheetProps) => {
   return (
     <>
       <Overlay isBlur={false} />
-      <BottomSheet minHeightVh={45} maxHeightVh={45}>
+      <BottomSheet minHeightPx={330} maxHeightPx={330}>
         <BottomSheet.Header />
         <div className="flex flex-col gap-10 pt-4 p-5 h-full">
           <div>
