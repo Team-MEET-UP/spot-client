ccaeeda1065e0d1bcc5d359cfe4c23316a18153f
yeona813 <aya813@naver.com>
2025-05-14 16:57:38 +0900

    [refactor] #60 : TMAP이 설치되지 않은 경우 APP store로 리다이렉트

--- src/features/mapView/utils/tMap.ts
+++ src/features/mapView/utils/tMap.ts
@@ -6,5 +6,14 @@ interface openTMapProps {
 
 export const openTMAP = ({ endPoint, endLat, endLog }: openTMapProps) => {
   const tMapUrl = `tmap://route?goalname=${endPoint}&goalx=${endLog}&goaly=${endLat}`;
-  window.open(tMapUrl, "_black");
+  const appStoreUrl = "https://apps.apple.com/kr/app/t-map/id431589174";
+
+  window.open(tMapUrl, "_blank");
+
+  // TMAP이 설치되지 않으면 APP store로 리다이렉트
+  setTimeout(() => {
+    if (!document.hidden) {
+      window.open(appStoreUrl, "_blank");
+    }
+  }, 1000);
 };
