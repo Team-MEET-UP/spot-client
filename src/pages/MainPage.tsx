79bad8d592d9853c98e42366c264cb025490b35c
yeona813 <aya813@naver.com>
2025-05-15 15:45:20 +0900

    [refactor] #81 : navigate 함수 분리

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -5,6 +5,10 @@ import { useNavigate } from "react-router-dom";
 const MainPage = () => {
   const navigate = useNavigate();
 
+  const handleClick = () => {
+    navigate("/find");
+  };
+
   return (
     <div className="relative bg-[#E5EFF7] h-screen-dvh flex flex-col justify-end">
       <div
@@ -12,12 +16,7 @@ const MainPage = () => {
         style={{ background: "linear-gradient(180deg, rgba(255, 255, 255, 0.00) 0%, #FFF 30%)" }}>
         <span className="text-md font-medium text-gray-60">모임의 기준을 만들다</span>
         <img src="/icon/logo.svg" alt="logo" className="mb-[38px]" />
-        <Button
-          onClick={() => {
-            navigate("/find");
-          }}>
-          중간지점 찾기
-        </Button>
+        <Button onClick={handleClick}>중간지점 찾기</Button>
         <KakaoLogin />
       </div>
       <img src="/image/main.png" alt="main" className="absolute top-16 right-0" />
