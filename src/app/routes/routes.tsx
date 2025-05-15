66a9812d350c8d8481b0f2a014c165e0936dd729
yeona813 <aya813@naver.com>
2025-05-15 13:57:42 +0900

    [refactor] #81 : routes import 절대경로로 수정

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -1,24 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
-import MapViewPage from "../../pages/MapViewPage";
-import MainPage from "../../pages/MainPage";
-import StartPointPage from "../../pages/StartPointPage";
-import FindPage from "../../pages/FindPage";
-import PlacePage from "../../pages/PlacePage";
-import DetailPage from "../../pages/DetailPage";
+import DetailPage from "@/pages/DetailPage";
+import FindPage from "@/pages/FindPage";
 import HistoryPage from "@/pages/HistoryPage";
-import ReviewPage from "@/pages/ReviewPage";
-import VisitedPage from "@/pages/VisitedPage";
+import MainPage from "@/pages/MainPage";
+import MapViewPage from "@/pages/MapViewPage";
 import MyPage from "@/pages/MyPage";
+import PlacePage from "@/pages/PlacePage";
+import ReviewPage from "@/pages/ReviewPage";
+import StartPointPage from "@/pages/StartPointPage";
+import VisitedPage from "@/pages/VisitedPage";
 
 export const router = createBrowserRouter([
   { path: "/", element: <MainPage /> },
-  { path: "/mapView", element: <MapViewPage /> },
-  { path: "/startPoint", element: <StartPointPage /> },
-  { path: "/find", element: <FindPage /> },
-  { path: "/place", element: <PlacePage /> },
   { path: "/detail", element: <DetailPage /> },
+  { path: "/find", element: <FindPage /> },
   { path: "/history", element: <HistoryPage /> },
-  { path: "/review/:id", element: <ReviewPage /> },
-  { path: "/visited/:id", element: <VisitedPage /> },
+  { path: "/mapView", element: <MapViewPage /> },
   { path: "/my", element: <MyPage /> },
+  { path: "/place", element: <PlacePage /> },
+  { path: "/review/:id", element: <ReviewPage /> },
+  { path: "/startPoint", element: <StartPointPage /> },
+  { path: "/visited/:id", element: <VisitedPage /> },
 ]);
