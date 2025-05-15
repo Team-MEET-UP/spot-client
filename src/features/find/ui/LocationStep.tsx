201b29369c906bbda44c139eb65bde3b7b558427
RohSungKyun <songkyun1108@naver.com>
2025-05-15 16:43:34 +0900

    [feat] #71 : 검색결과 없을 경우 ui

--- src/features/find/ui/LocationStep.tsx
+++ src/features/find/ui/LocationStep.tsx
@@ -98,6 +98,15 @@ export const LocationStep = ({ setCurrentStep, startPointInfo, setStartPointInfo
             <div className="flex flex-col gap-2 overflow-y-auto max-h-[calc(100vh-216px)] [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
               {isError ? (
                 <p className="text-red-500 text-sm">검색 중 오류가 발생했어요.</p>
+              ) : searchResults.length === 0 ? (
+                <div className="flex flex-col items-center justify-center py-10">
+                  <img src="/icon/noresult.svg" alt="검색 결과 없음" />
+                  <p className="text-center text-gray-40 text-sm">
+                    일치하는 주소가 없어요
+                    <br />
+                    서울 내 지역인지 다시 확인해보세요
+                  </p>
+                </div>
               ) : (
                 searchResults.map((location, index) => (
                   <LocationCard
