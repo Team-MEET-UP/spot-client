67659ce01e60fc5ee5d0d4884b52d3c24cfaba33
yeona813 <aya813@naver.com>
2025-05-16 01:32:50 +0900

    [fix] #83 : icon 크기 수정

--- src/features/detail/ui/PlaceInfo.tsx
+++ src/features/detail/ui/PlaceInfo.tsx
@@ -8,7 +8,7 @@ export const PlaceInfo = () => {
   return (
     <div className="px-5 pt-4 pb-[14px] flex flex-col gap-[6px]">
       <div className="flex py-[2px] px-[6px] gap-1 items-center rounded-[4px] bg-sub-10 w-fit text-xs font-semibold text-sub-sub">
-        <img src={Pin} alt="pin" className="w-[4px] h-[14px]" />
+        <img src={Pin} alt="pin" className="w-[14px] h-[14px]" />
         역에서 215m
       </div>
       <div>
@@ -20,7 +20,7 @@ export const PlaceInfo = () => {
       </div>
       <div className="flex justify-between">
         <div className="flex items-center gap-1 text-sm font-medium">
-          <img src={Time} alt="time" className="w-[4px] h-[14px]" />
+          <img src={Time} alt="time" className="w-[14px] h-[14px]" />
           <p className="text-gray-50">영업시간</p>
           <p className="text-gray-80 ml-1">07:30 ~ 22:00</p>
         </div>
