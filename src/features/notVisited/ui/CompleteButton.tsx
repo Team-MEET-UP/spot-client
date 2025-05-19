0eb014bec15ec5706b3e6258e0d3a0ff3a6130ce
yeona813 <aya813@naver.com>
2025-05-19 18:07:05 +0900

    [fix] #116 : 버튼 높이 수정

--- src/features/notVisited/ui/CompleteButton.tsx
+++ src/features/notVisited/ui/CompleteButton.tsx
@@ -7,7 +7,7 @@ type CompleteButtonProps = {
 export const CompleteButton = ({ label, onClick, disabled }: CompleteButtonProps) => {
   return (
     <button
-      className={`w-full h-[56px] text-white transition text-lg font-semibold rounded-xl
+      className={`w-full h-[52px] text-white transition text-lg font-semibold rounded-xl
     ${disabled ? "bg-gray-10 text-gray-30 cursor-not-allowed" : "bg-gray-90"}
   `}
       disabled={disabled}
