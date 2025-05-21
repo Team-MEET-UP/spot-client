ac4d28a28975b13d727a236016401ece68041519
yeona813 <aya813@naver.com>
2025-05-21 10:14:12 +0900

    [feat] #126 : 유저 모임 리스트 get 요청 API 구현

--- src/features/history/service/api.ts
+++ src/features/history/service/api.ts
@@ -37,3 +37,17 @@ export const storeUserAgreement = async ({
     console.error("서버 에러 : ", error);
   }
 };
+
+export const getUserEvents = async () => {
+  try {
+    const response = await api.get("/users/events");
+    if (response.data.result === "SUCCESS") {
+      return response.data.data;
+    } else {
+      console.error("유저 모임 조회 실패 : ", response.data.error.message);
+      throw new Error(response.data.error.message);
+    }
+  } catch (error) {
+    console.error("서버 에러 : ", error);
+  }
+};
