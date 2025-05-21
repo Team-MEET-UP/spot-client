b0ae1e87fd4ee6605ca13d0165c47472d659c151
yeona813 <aya813@naver.com>
2025-05-21 10:14:25 +0900

    [feat] #126 : 유저 모임 리스트 get 요청 API hook 구현

---
+++ src/features/history/hooks/useUserEvents.ts
@@ -0,0 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import { getUserEvents } from "../service";
+
+interface UserEvents {
+  eventId: string;
+  middlePointName: string;
+  placeName: string;
+  participatedPeopleCount: number;
+  userProfileImageUrls: string[];
+  eventMadeAgo: number;
+  isReviewed: boolean;
+}
+
+export const useUserEvents = () => {
+  const {
+    data: userEvents,
+    isLoading: isEventsLoading,
+    isError: isEventsError,
+  } = useQuery<UserEvents[]>({
+    queryKey: ["userEvents"],
+    queryFn: getUserEvents,
+    retry: 2, // 실패 시 최대 두 번 재시도
+    staleTime: 1000 * 60 * 5, // 5분 동안 데이터 캐시 유지
+  });
+  return { userEvents, isEventsLoading, isEventsError };
+};
