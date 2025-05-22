46fc0bc230241175c1b6546139ae05a61d0f9ce7
RohSungKyun <songkyun1108@naver.com>
2025-05-22 23:34:06 +0900

    [feat] #135 : 리뷰 진입페이지 props 정리

--- src/pages/ReviewPage.tsx
+++ src/pages/ReviewPage.tsx
@@ -1,21 +1,35 @@
 import PlaceCard from "@/shared/ui/PlaceCard";
-import { mockPlaceItems } from "@/shared/model/mocks/mockPlaceItems";
 import { useNavigate, useParams } from "react-router-dom";
 import { SmallButton, Title } from "@/features/review/ui";
 import { CloseHeader } from "@/widgets/headers";
+import { useReviewPlace } from "@/features/review/hooks";
 
 const ReviewPage = () => {
   const { id } = useParams();
   const navigate = useNavigate();
+
+  const { data, isLoading, isError } = useReviewPlace(id!);
+  if (isLoading) return <p className="p-5">로딩 중...</p>;
+  if (isError || !data) return <p className="p-5">데이터를 불러오는 데 실패했어요.</p>;
+
+  const { confirmedPlaceResponse } = data;
 
   return (
     <div className="flex flex-col h-screen-dvh">
       <CloseHeader url="/history" />
       <div className="flex flex-col justify-between h-full p-5 pt-4">
         <div className="flex flex-col gap-8">
-          <Title title="박승광해물손칼국수 진점점dddddd 박승광해물" />
+          <Title title={confirmedPlaceResponse.name} />
           <div className="flex flex-col gap-3 items-center">
-            <PlaceCard {...mockPlaceItems.data.placeResponses[0]} /> {/* 임시 데이터 추가 */}
+            <PlaceCard
+              name={confirmedPlaceResponse.name}
+              distance={confirmedPlaceResponse.distance}
+              image={confirmedPlaceResponse.image}
+              openTime={confirmedPlaceResponse.openTime}
+              closeTime={confirmedPlaceResponse.closeTime}
+              averageRating={confirmedPlaceResponse.averageRating}
+              placeScore={confirmedPlaceResponse.placeScore}
+            />{" "}
             <p className="text-sm font-semibold text-gray-30">실제로 가신 곳인지 확인해주세요.</p>
           </div>
         </div>
