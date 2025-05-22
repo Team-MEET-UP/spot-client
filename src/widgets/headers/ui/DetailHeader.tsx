b5a5a18f2878d21be4357477312ace0292571a16
yeona813 <aya813@naver.com>
2025-05-22 18:44:17 +0900

    [feat] #134 : Header Props 전달

--- src/widgets/headers/ui/DetailHeader.tsx
+++ src/widgets/headers/ui/DetailHeader.tsx
@@ -7,18 +7,19 @@ interface DetailHeaderProps {
   backClick: () => void;
   shareClick: () => void;
   isScrolled: boolean;
+  name: string;
 }
 
-// 추후
-export const DetailHeader = ({ backClick, shareClick, isScrolled }: DetailHeaderProps) => {
+export const DetailHeader = ({ backClick, shareClick, isScrolled, name }: DetailHeaderProps) => {
   return (
-    <div className={`flex justify-between ${isScrolled ? "bg-white px-5 py-3" : "absolute top-3 left-5 right-5"}`}>
+    <div
+      className={`flex justify-between z-[1003] ${isScrolled ? "bg-white px-5 py-3" : "absolute top-3 left-5 right-5"}`}>
       <button onClick={backClick}>
         <img src={isScrolled ? BackScroll : Back} alt="back" className="w-6 h-6" />
       </button>
       {isScrolled && (
         <span className="text-lg font-semibold text-gray-90 w-[252px] overflow-hidden text-ellipsis whitespace-nowrap">
-          스타벅스스타벅스방배거리점스타벅스
+          {name}
         </span>
       )}
       <button onClick={shareClick}>
