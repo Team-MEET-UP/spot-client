38b8e6f63137faf346a39105e8122aa29ef85a68
RohSungKyun <songkyun1108@naver.com>
2025-05-23 13:37:24 +0900

    [refactor] #135 : smallButton navigate 함수 분리

--- src/features/review/ui/SmallButton.tsx
+++ src/features/review/ui/SmallButton.tsx
@@ -8,11 +8,15 @@ interface SmallButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>
 
 export const SmallButton = ({ children, isVisit, placeId }: SmallButtonProps) => {
   const navigate = useNavigate();
+  const handleClick = () => {
+    const path = `/${isVisit ? "visited" : "notVisited"}/${placeId}`;
+    navigate(path);
+  };
 
   return (
     <button
       className={`w-full py-4 text-center text-md font-semibold rounded-xl ${isVisit ? "bg-gray-90 text-white" : "bg-gray-10 text-gray-60"}`}
-      onClick={() => navigate(`/${isVisit ? "visited" : "notVisited"}/${placeId}`)}>
+      onClick={() => handleClick()}>
       {children}
     </button>
   );
