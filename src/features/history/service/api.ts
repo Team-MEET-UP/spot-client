8168d4887f74597dccc31527d964be1b8bbd4cf6
yeona813 <aya813@naver.com>
2025-05-23 10:00:36 +0900

    [refactor] #138 : HI 불필요한 try-catch 제거

--- src/features/history/service/api.ts
+++ src/features/history/service/api.ts
@@ -1,16 +1,10 @@
 import api from "@/shared/api/api";
 
 export const getUserInfo = async () => {
-  try {
-    const response = await api.get("/users");
-    if (response.data.result === "SUCCESS") {
-      return response.data.data;
-    } else {
-      console.error("유저 정보 조회 실패 : ", response.data.error.message);
-      throw new Error(response.data.error.message);
-    }
-  } catch (error) {
-    console.error("서버 에러 : ", error);
+  const response = await api.get("/users");
+
+  if (response.data.result === "SUCCESS") {
+    return response.data.data;
   }
 };
 
@@ -21,33 +15,24 @@ export const storeUserAgreement = async ({
   isPersonalInfoAgreement: boolean;
   isMarketingAgreement: boolean;
 }) => {
-  try {
-    const response = await api.post("/users/agreement", {
-      isPersonalInfoAgreement,
-      isMarketingAgreement,
-    });
+  const response = await api.post("/users/agreement", {
+    isPersonalInfoAgreement,
+    isMarketingAgreement,
+  });
 
-    if (response.data.result === "SUCCESS") {
-      return true;
-    } else {
-      console.error("약관 정보 동의 실패 : ", response.data.error.message);
-      throw new Error(response.data.error.message);
-    }
-  } catch (error) {
-    console.error("서버 에러 : ", error);
+  if (response.data.result === "SUCCESS") {
+    return true;
   }
+
+  throw new Error(response.data.error?.message || "약관 동의 저장장 실패");
 };
 
 export const getUserEvents = async () => {
-  try {
-    const response = await api.get("/users/events");
-    if (response.data.result === "SUCCESS") {
-      return response.data.data;
-    } else {
-      console.error("유저 모임 조회 실패 : ", response.data.error.message);
-      throw new Error(response.data.error.message);
-    }
-  } catch (error) {
-    console.error("서버 에러 : ", error);
+  const response = await api.get("/users/events");
+
+  if (response.data.result === "SUCCESS") {
+    return response.data.data;
   }
+
+  throw new Error(response.data.error?.message || "유저 정보 조회 실패");
 };
