c1aad2304f6387f89d7c22ec7587be36b7f8414b
RohSungKyun <songkyun1108@naver.com>
2025-05-24 23:16:11 +0900

    [feat] #141 : guestId 로그인 사용자 조건 처리

--- src/features/mapView/hooks/useEventInfo.ts
+++ src/features/mapView/hooks/useEventInfo.ts
@@ -2,16 +2,19 @@ import { useQuery } from "@tanstack/react-query";
 import { getCookie } from "@/shared/utils";
 import { getEventInfo } from "../service";
 import { useParams } from "react-router-dom";
+import { useUserStore } from "@/shared/stores";
 
 export const useEventRoutes = () => {
   const { id } = useParams();
-  const startPointId = getCookie("startPointId");
+  const guestId = getCookie("guestId");
+  const { nickname } = useUserStore();
 
   return useQuery({
-    queryKey: ["eventRoutes", id, startPointId],
+    queryKey: ["eventRoutes", id, guestId, nickname],
     queryFn: () => {
       if (!id) throw new Error("eventId가 없습니다.");
-      return getEventInfo(id, startPointId);
+      // 로그인한 사용자인 경우 guestId를 전달하지 않음
+      return getEventInfo(id, nickname ? undefined : guestId);
     },
     enabled: !!id, // eventId가 있을 때만 요청
     retry: false, // 자동 재시도 방지
