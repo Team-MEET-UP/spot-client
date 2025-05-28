0a78ff017419b1ed1161105743af9ec36b6c34ae
RohSungKyun <songkyun1108@naver.com>
2025-05-28 13:42:04 +0900

    [fix] #169 : 에러 바텀시트 ui 높이 조정

--- src/features/mapView/ui/bottomsheets/TooCloseSheet.tsx
+++ src/features/mapView/ui/bottomsheets/TooCloseSheet.tsx
@@ -9,7 +9,7 @@ export const TooCloseSheet = () => {
       <Overlay />
       <BottomSheet>
         <BottomSheet.Content>
-          <div className="flex flex-col justify-center items-center h-full">
+          <div className="flex flex-col justify-start items-center h-full pt-8">
             <img src={Error} alt="memeberError" className="w-[160px] h-[160px]" />
             <h1 className="text-gray-80 font-bold text-lg">가까워서 중간지점을 찾을 수 없어요</h1>
             <p className="text-gray-40 text-sm">멤버를 추가하거나 출발지를 수정해주세요</p>
